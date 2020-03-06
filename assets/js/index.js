825aeb23404413c170117dd6423984c4cb9634f7
Derek Glassick <derek.glassick@gmail.com>
2020-03-05 16:52:40 -0800

    added css and start to pseudo code the js.

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// create variables for time, questions, etc.
+// create appendchild sections for questions
+// for each question create if/else statments where if statement is for true answer else statements for wrong
+// add time penalty if wrong answer is selected.
+
+var timeCounter = 180;
+var scoreCounter = 0;
