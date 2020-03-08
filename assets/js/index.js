3e829ffeaac9a0b5d51c646a967007fc0fcada30
Derek Glassick <derek.glassick@gmail.com>
2020-03-08 13:43:38 -0700

    added css so the buttons change color based on mouse hover. Was not able to get timer or score working. Function also gets confused and doesn't run through all the answers.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,51 @@
 
 var timeCounter = 180;
 var scoreCounter = 0;
+var startButton = document.getElementById("startButton");
+var currentIndex = 0;
+var quizQuestion = document.getElementById("quiz-question");
+var quizAnswers = document.getElementById("quiz-answers");
+
+startButton.onclick = startQuiz;
+
+function startQuiz(){
+    
+var startScreenEl = document.getElementById("startScreen");
+startScreenEl.setAttribute("hidden", true);
+quizQuestion.removeAttribute("class");
+getQuestions();
+
+};
+
+function getQuestions(){
+var currentQuestion = questionArray[currentIndex];
+var questionTitle = document.getElementById("question-title");
+questionTitle.textContent = currentQuestion.title;
+questionTitle.setAttribute("question", currentIndex);
+// currentQuestion.options for loop with createElement button to 
+for ( var i = 0 ; i < currentQuestion.options.length; i++){    
+var currentAnswers = document.createElement('button');
+// currentAnswers.setAttribute('text');
+currentAnswers.textContent = currentQuestion.options[i];
+console.log(currentQuestion.options[i])
+quizAnswers.appendChild(currentAnswers);
+};
+};
+
+// quizAnswers.onclick = selectAnswer
+// function selectAnswer(){
+//     if (currentQuestion.options[i] = currentQuestion.answer)
+//     alert("You're right!");
+//     else{
+//         alert("Nope!");
+//     }};
+
+    // document.querySelectorAll("#quiz-answers button")
+    quizAnswers.addEventListener("click", function(e) {
+        if (e.target.textContent === questionArray[currentIndex].answer)
+    alert("You're right!");
+    else{
+        alert("Nope!");
+        console.log(e.target.textContent === questionArray[currentIndex].answer);
+    
+    }});
---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+var questionArray = [{
+    title: "Who was the 1st president of the US?",
+options: ["George Washington", "Abraham Lincoln", "John Adams", "Theodore Roosevelt"],
+answer: "George Washington"
+},
+{
+    title: "Next question",
+    options: ["a", "b", "c"],
+answer: "a"
+},
+{
+},];
+// title:
+// options:
+// answer:
+
