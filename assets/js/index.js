3519080fed848a24f6f00225e919878f4b9db8aa
Derek Glassick <derek.glassick@gmail.com>
2020-03-09 18:03:46 -0700

    fixed css and moved function

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@
 // for each question create if/else statments where if statement is for true answer else statements for wrong
 // add time penalty if wrong answer is selected.
 
+var timerEl = document.getElementById("seconds-timer");
 var timeCounter = 180;
 var scoreCounter = 0;
 var startButton = document.getElementById("startButton");
@@ -10,31 +11,68 @@ var currentIndex = 0;
 var quizQuestion = document.getElementById("quiz-question");
 var quizAnswers = document.getElementById("quiz-answers");
 
+// startButton triggers the startQuiz function
 startButton.onclick = startQuiz;
 
-function startQuiz(){
-    
-var startScreenEl = document.getElementById("startScreen");
-startScreenEl.setAttribute("hidden", true);
-quizQuestion.removeAttribute("class");
-getQuestions();
+// startQuiz function identifies the variable startScreenEl by grabbing the html div startScreen
+// replaces startScreen attribute and made it hidden
+// quizQuestion attribute is then set to default
+// finally runs getQuestions function
+function startQuiz() {
+    var startScreenEl = document.getElementById("startScreen");
+    startScreenEl.setAttribute("hidden", true);
+    quizQuestion.removeAttribute("class");
+
+    getQuestions();
 
 };
 
-function getQuestions(){
-var currentQuestion = questionArray[currentIndex];
-var questionTitle = document.getElementById("question-title");
-questionTitle.textContent = currentQuestion.title;
-questionTitle.setAttribute("question", currentIndex);
-// currentQuestion.options for loop with createElement button to 
-for ( var i = 0 ; i < currentQuestion.options.length; i++){    
-var currentAnswers = document.createElement('button');
-// currentAnswers.setAttribute('text');
-currentAnswers.textContent = currentQuestion.options[i];
-console.log(currentQuestion.options[i])
-quizAnswers.appendChild(currentAnswers);
+function getQuestions() {
+    var currentQuestion = questionArray[currentIndex];
+    var questionTitle = document.getElementById("question-title");
+    questionTitle.textContent = currentQuestion.title;
+    questionTitle.setAttribute("question", currentIndex);
+
+    quizAnswers.innerHTML = ""
+
+
+
+    // currentQuestion.options for loop with createElement button to 
+    for (var i = 0; i < currentQuestion.options.length; i++) {
+        var currentAnswers = document.createElement('button');
+        // currentAnswers.setAttribute('text');
+        currentAnswers.textContent = currentQuestion.options[i];
+        console.log(currentQuestion.options[i])
+        quizAnswers.appendChild(currentAnswers);
+    };
 };
-};
+quizAnswers.addEventListener("click", function (e) {
+    if (e.target.textContent === questionArray[currentIndex].answer) {
+        alert("CORRECT");
+        // currentIndex++;
+    }
+    else {
+        alert("Nope!");
+        // currentIndex++;
+        // getQuestions();
+        console.log(e.target.textContent === questionArray[currentIndex].answer);
+    }
+    currentIndex++;
+    getQuestions();
+
+});
+
+// function timerFunction(){
+//     timeCounter = timeCounter -1;
+//     if(timeCounter < 180){
+//     ti;merEl.innerHTML = timeCounter;
+//     }
+//     if (timeCounter < 1){
+//         window.clearInterval(update)
+//     };
+// };
+// update = setInterval(timerFunction(), 1000);
+//     getQuestions();
 
 // quizAnswers.onclick = selectAnswer
 // function selectAnswer(){
@@ -44,12 +82,11 @@ quizAnswers.appendChild(currentAnswers);
 //         alert("Nope!");
 //     }};
 
-    // document.querySelectorAll("#quiz-answers button")
-    quizAnswers.addEventListener("click", function(e) {
-        if (e.target.textContent === questionArray[currentIndex].answer)
-    alert("You're right!");
-    else{
-        alert("Nope!");
-        console.log(e.target.textContent === questionArray[currentIndex].answer);
-    
-    }});
+// document.querySelectorAll("#quiz-answers button")
+setTimeout(add,10000);
+
+function add(){
+
+
+
+}
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,16 +1,33 @@
+// questionArray is an array with objects. Each object represents question, answer options, and answer. 
+// The function getQuestions on the index.js file will pull the objects and input them on the html page.
 var questionArray = [{
-    title: "Who was the 1st president of the US?",
-options: ["George Washington", "Abraham Lincoln", "John Adams", "Theodore Roosevelt"],
-answer: "George Washington"
-},
-{
-    title: "Next question",
-    options: ["a", "b", "c"],
-answer: "a"
-},
-{
-},];
-// title:
-// options:
-// answer:
-
+        title: "Who was the 1st president of the US?",
+        options: ["George Washington", "Abraham Lincoln", "John Adams", "Theodore Roosevelt"],
+        answer: "George Washington",
+    },
+    {
+        title: "Who wrote the science fiction series Dune?",
+        options: ["George Lucas", "Ray Bradbury", "Frank Herbert", "Philip K Dick"],
+        answer: "Frank Herbert",
+    },
+    {
+        title: "What does All-Might scream when fighting All-for-One?",
+        options: ["Time to finish you!", "United States of Smaaaaaaaash!", "Pikachu! I choose you!", "Kamehameha!!"],
+        answer: "United States of Smaaaaaaaash!",
+    },
+    {
+        title: "What was the nickname for Harry Potter?",
+        options: ["the lightning bolt kid","the one that didn't die","he-who-shall-be-named","The boy who lived"],
+        answer: "The boy who lived",
+    },
+    {
+        title: "What game won Game of the Year in 2019?",
+        options: ["Sekiro: Shadows Die Twice","The Outerworlds","God of War","Control"],
+        answer: "Sekiro: Shadows Die Twice",
+    },
+    {
+        title: "What did Darth Vader say to Luke Skywalker in Episode V?",
+        options: ["Luke, I am your father","Join me and we can rule the planet","I got your hand","No, I am your father"],
+        answer: "No, I am your father",
+    },
+];
